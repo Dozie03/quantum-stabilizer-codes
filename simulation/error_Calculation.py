1ff7ac214f91e41770fd1b65074f6561e7532f00
Bisaam Hassan <bisaam221@gmail.com>
2024-11-21 13:07:48 -0600

    trying to add gitignore

--- simulation/error_Calculation.py
+++ simulation/error_Calculation.py
@@ -2,37 +2,72 @@ import stim
 import numpy as np
 from stimbposd import BPOSD
 
-def generate_stim_circuit(stabilizers, p, num_rounds):
+def generate_stim_circuit(stabilizers, p, num_rounds, logical_x):
     circuit = stim.Circuit()
-    circuit.append_operation("R", range(9))
+    num_data_qubits = len(stabilizers[0])
+    num_stabilizers = len(stabilizers)
+    num_total_qubits = num_data_qubits + num_stabilizers
 
+    # Define QUBIT_COORDS with only 2 parameters
+    for i in range(num_data_qubits):
+        circuit.append("QUBIT_COORDS", [i], [0, i])
+    for i in range(num_stabilizers):
+        circuit.append("QUBIT_COORDS", [num_data_qubits + i], [1, i])
+
+    # Initialize all qubits
+    circuit.append_operation("R", range(num_total_qubits))
+
+    # Apply initial X errors to all qubits
+    circuit.append_operation("X_ERROR", range(num_total_qubits), p)
+
+    # Create the repeated part of the circuit
     for round in range(num_rounds):
-        for q in range(5):
-            circuit.append_operation("X_ERROR", [q], p)
-
+        # Measure stabilizers
         for i, stabilizer in enumerate(stabilizers):
-            circuit.append("H", 5 + i)
-            
+            ancilla = num_data_qubits + i
+            circuit.append("H", ancilla)
             for j, pauli in enumerate(stabilizer):
                 if pauli == 'X':
-                    circuit.append("CNOT", [5 + i, j])
+                    circuit.append("CX", [ancilla, j])
                 elif pauli == 'Z':
-                    circuit.append("CZ", [5 + i, j])
+                    circuit.append("CZ", [ancilla, j])
                 elif pauli == 'Y':
-                    circuit.append("S_DAG", j)
-                    circuit.append("CNOT", [5 + i, j])
-                    circuit.append("S", j)
-            
-            circuit.append_operation("M", [5+i])
-            circuit.append_operation("DETECTOR", [], [5+i])
-            circuit.append("H", 5 + i)
+                    circuit.append("CY", [ancilla, j])
+            circuit.append("H", ancilla)
+            circuit.append_operation("M", [ancilla])
 
-        for q in range(5):
-            circuit.append_operation("Z_ERROR", [q], p)
+        # Apply X errors to all qubits
+        circuit.append_operation("X_ERROR", range(num_total_qubits), p)
 
-    circuit.append_operation("M", range(5))
-    circuit.append_operation("OBSERVABLE_INCLUDE", [stim.target_rec(-5), stim.target_rec(-4), stim.target_rec(-3), stim.target_rec(-2), stim.target_rec(-1)], 0)
-    circuit.append_operation("OBSERVABLE_INCLUDE", [stim.target_rec(-5), stim.target_rec(-4), stim.target_rec(-3), stim.target_rec(-2), stim.target_rec(-1)], 1)
+        # Add DETECTOR for each stabilizer measurement
+        if round == 0:  # First round
+            for i in range(num_stabilizers):
+                circuit.append("DETECTOR", [stim.target_rec(-1-i)], [1, i, 0])
+        else:
+            # Add SHIFT_COORDS before DETECTOR
+            circuit.append("SHIFT_COORDS", [], [0, 0, 1])
+            for i in range(num_stabilizers):
+                circuit.append("DETECTOR", [
+                    stim.target_rec(-1-i),
+                    stim.target_rec(-1-i-num_stabilizers)
+                ], [1, i, 0])
+
+    # Measure data qubits at the end
+    circuit.append_operation("M", range(num_data_qubits))
+
+    # Add SHIFT_COORDS before final DETECTOR
+    circuit.append("SHIFT_COORDS", [], [0, 0, 1])
+
+    # Add final DETECTOR for each stabilizer
+    for i in range(num_stabilizers):
+        circuit.append("DETECTOR", [
+            stim.target_rec(-1-i-num_data_qubits),
+            stim.target_rec(-1-i-num_data_qubits-num_stabilizers)
+        ], [1, i, 1])
+
+    # Add OBSERVABLE_INCLUDE for logical X operator
+    logical_x_indices = [i for i, x in enumerate(logical_x) if x == '1']
+    circuit.append("OBSERVABLE_INCLUDE", [stim.target_rec(-i-1) for i in logical_x_indices], 0)
 
     return circuit
 
@@ -76,36 +111,33 @@ def convert_to_stabilizers(x_part, z_part):
         stabilizers.append(stabilizer)
     return stabilizers
 
+
+
 def main():
-    # stabilizers = [
-    #     "XIYZX",
-    #     "ZIXYZ", 
-    #     "IXXXX",
-    #     "IZZZZ"
-    # ]
     x_part = [
         [1, 0, 1, 0, 1],
         [0, 0, 1, 1, 0],
         [0, 1, 1, 1, 1],
         [0, 0, 0, 0, 0]
     ]
-    
     z_part = [
         [0, 0, 1, 1, 0],
         [1, 0, 0, 1, 1],
         [0, 0, 0, 0, 0],
         [0, 1, 1, 1, 1]
     ]
+
     x_part = np.array(x_part)
     z_part = np.array(z_part)
     stabilizers = convert_to_stabilizers(x_part, z_part)
     print(stabilizers)
 
     p = 0.07
-    num_rounds = 10
+    num_rounds = 3
     num_shots = 100
 
-    circuit = generate_stim_circuit(stabilizers, p, num_rounds)
+    # circuit = generate_stim_circuit(stabilizers, p, num_rounds, '11111', '11111')
+    circuit = generate_stim_circuit(stabilizers, p, num_rounds, '11111')
     print("Circuit:")
     print(circuit)
 
@@ -124,7 +156,7 @@ def main():
         print(f"Shot {i+1}: {predicted_observables[i]}")
 
     error_rate, num_mistakes = calculate_error_rate(predicted_observables, observables)
-    print(f"Error rate: {num_mistakes}/{num_shots} ({error_rate:.2%})")
+    print(f"\nError rate: {num_mistakes}/{num_shots} ({error_rate:.2%})")
 
 if __name__ == "__main__":
     main()
